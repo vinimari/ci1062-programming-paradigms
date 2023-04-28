1a47b48d5531692563498b18417fab6de2c36de7
vinimari <viniciusmarinho44@hotmail.com>
2023-04-28 14:07:37 -0300

    second problem resolved

--- Semana 1/exercicios_sem1/src/App.java
+++ Semana 1/exercicios_sem1/src/App.java
@@ -5,23 +5,43 @@ public class App {
     public static void main(String[] args) throws Exception {
         // 1. Construa um programa em Java que leia um número e diga se ele é positivo.
         resolveProblem1();
+
+        // 2. Construa um programa em Java que leia um número inteiro e diga se ele é par ou ímpar
+        resolveProblem2();
+    }
+
+    static void resolveProblem2() { 
+        Scanner scanner = new Scanner(System.in); 
+        int number; 
+        String answer = " it's even"; 
+        
+        System.out.println("\n## Problem 2 ##");
+        System.out.print("Enter an integer value: ");
+        number = scanner.nextInt();
+
+        if (number % 2 == 0) { 
+            answer = " it's odd";
+        }
+
+        System.out.println(number+answer);
     }
 
     static void resolveProblem1() {
         Scanner scanner = new Scanner(System.in);
         int number;
-        String answer = "O número é nulo";
+        String answer = " it's null";
 
-        System.out.println("Digite um número inteiro: ");
+        System.out.println("\n## Problem 1 ##");
+        System.out.print("Enter an integer value: ");
         number = scanner.nextInt();
 
         if (number > 0) {
-            answer = "O número é positivo";
+            answer = " it's positive";
         } else {
-            answer = "O número é negativo";
+            answer = " it's negative";
         }
 
-        System.out.println(answer);
+        System.out.println(number+answer);
     }
 
 }
