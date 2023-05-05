edf3e28066116bc961c0225d19f9f14904d4016f
vinimari <viniciusmarinho44@hotmail.com>
2023-05-04 23:58:06 -0300

    exercise 2 done

--- Semana 2/exercicios_sem2/src/App.java
+++ Semana 2/exercicios_sem2/src/App.java
@@ -4,8 +4,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         Person personOne = new Person("Grace Hopper", 9, 12, 1906);
         Person personTwo = new Person("Ada Lovelace", 10, 12, 1815);
+        Person[] persons = { personOne, personTwo };
 
-        personOne.calculateAge(4, 5, 2023);
-        personTwo.calculateAge(4, 5, 2023);
+        for (int i = 0; i < 2; i++) {
+            persons[i].calculateAge(4, 5, 2023);
+        }
     }
 }
